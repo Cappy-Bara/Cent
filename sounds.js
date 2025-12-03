7cece81c8ebc61f48c656e5ada91ead68c19c6f8
cappy_bara <kacper916@gmail.com>
2025-12-03 01:33:01 +0100

    first working version

---
+++ sounds.js
@@ -0,0 +1,18 @@
+function getDetunedUpperSoundFrequency(frequency, cents) {
+  var nextTone = getNextFrequency(frequency);
+  var freqDiff = nextTone - frequency;
+  var centileDiff = freqDiff / 100;
+  var newFreq = frequency + (centileDiff * cents);
+  return newFreq;
+}
+
+function getNextFrequency(currentFrequency) {
+  const val = 1 / 12;
+  return currentFrequency * Math.pow(2, val);
+}
+
+function playSound(freq, len) {
+  console.log(freq)
+  const osc = new Tone.Oscillator(freq, "sine").toDestination().start();
+  setTimeout(() => { osc.stop(); osc.dispose(); }, len);
+}
